2e8647d61f8e6cf564f9a565159814440b130750
Fuzyn <michalwierzbicki.miw@gmail.com>
2024-08-21 20:39:36 +0200

    update hello with fake progress

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,8 +1,27 @@
-import {Card, CardActions, CardContent, Grid, Typography} from "@mui/material";
+import {Card, CardActions, CardContent, Grid, LinearProgress, Typography} from "@mui/material";
 import {LoadingButton} from "@mui/lab";
 import {ArrowRightAltOutlined} from "@mui/icons-material";
+import {useEffect, useState} from "react";
 
 const Hello = (props) => {
+    const [fakeProgress, setFakeProgress] = useState(0)
+
+    const loadResume = () => {
+        setFakeProgress(10)
+    }
+
+    useEffect(() => {
+        if (fakeProgress > 0) {
+            if (fakeProgress < 100) {
+                const timerId = setInterval(() => setFakeProgress(fakeProgress + 10), 100);
+
+                return () => clearInterval(timerId);
+            } else {
+                setTimeout(() => props.showPortfolio(), 300)
+            }
+        }
+    }, [fakeProgress])
+
     return <Grid minHeight="100vh" alignContent="center">
         <Card variant="outlined" sx={{backgroundColor: "rgba(0,0,0,0.4)", color: "white"}}>
             <CardContent>
@@ -11,10 +30,16 @@ const Hello = (props) => {
                 <Typography variant="subtitle1" textAlign="center">Click on the button below to view my CV and portfolio</Typography>
             </CardContent>
             <CardActions sx={{justifyContent: "center"}}>
-                <LoadingButton variant="outlined" sx={{minWidth: "200px"}} onClick={() => props.showPortfolio()}>
+                <LoadingButton
+                    variant="contained"
+                    sx={{minWidth: "200px"}}
+                    onClick={() => loadResume()}
+                    loading={fakeProgress > 0}
+                >
                     <ArrowRightAltOutlined/>
                 </LoadingButton>
             </CardActions>
+            {fakeProgress > 0 && <LinearProgress variant="determinate" value={fakeProgress}/>}
         </Card>
     </Grid>
 }
