44bd2d0cf026fda305ddfe5a7ac8031492071926
michaelWierzbicki <michael.wierzbicki@gdsoft.info>
2024-08-21 19:20:10 +0200

    Add material UI, change in Hello.js

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,17 +1,24 @@
 import arrow from '../assets/arrow.png';
+import {Card, CardActions, CardContent, Grid, Paper, Typography} from "@mui/material";
+import {LoadingButton} from "@mui/lab";
+import {ArrowRightAltOutlined} from "@mui/icons-material";
 
 
 const Hello = (props) => {
-    return <div className='hello-div'>
-        <div className='hello-text'>
-            <h1 className="hiText-Hi">Hi!</h1>
-            <h1 className="hiText-rest">My name is Michał Wierzbicki</h1>
-            <h1 className="hiText-rest">Click on the button below to view my CV and portfolio</h1>
-            <div className='button_hello_div'>
-                <button className='button_hello' onClick={() => props.showPortfolio()}><img src={arrow} alt='arrow' className='arrow' /></button>
-            </div>
-        </div>
-    </div>
+    return <Grid minHeight="100vh" alignContent="center">
+        <Card variant="outlined" sx={{backgroundColor: "rgba(0,0,0,0.4)", color: "white"}}>
+            <CardContent>
+                <Typography variant="h1" textAlign="center" sx={{textShadow: "7px 7px #000"}}>Hi!</Typography>
+                <Typography variant="subtitle1" textAlign="center">My name is Michał Wierzbicki</Typography>
+                <Typography variant="subtitle1" textAlign="center">Click on the button below to view my CV and portfolio</Typography>
+            </CardContent>
+            <CardActions sx={{justifyContent: "center"}}>
+                <LoadingButton variant="outlined" sx={{minWidth: "200px"}} onClick={() => props.showPortfolio()}>
+                    <ArrowRightAltOutlined/>
+                </LoadingButton>
+            </CardActions>
+        </Card>
+    </Grid>
 }
 
 export default Hello;
