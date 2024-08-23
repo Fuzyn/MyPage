972434c4f41c0b56ab3ac80884aea8248990c260
Fuzyn <michalwierzbicki.miw@gmail.com>
2024-08-23 19:10:11 +0200

    rebuild example file

---
+++ src/components/PotfrolioPages/Example.js
@@ -0,0 +1,20 @@
+import React from "react";
+import github from "../../assets/skills/github.png";
+import {Grid, Typography} from "@mui/material";
+
+const Example = ({appLink, gitLink, srcImg, altImg, title, description}) => {
+    return <Grid container data-aos="fade-left" padding="20px">
+        <Grid item xs={5}>
+            <a href={appLink}>
+                <img src={srcImg} className="examples-photo" alt={altImg} />
+            </a>
+        </Grid>
+        <Grid item xs={7}>
+            <Typography variant="h5">{title}</Typography>
+            <Typography variant="h7">{description}</Typography>
+        </Grid>
+        <Grid xs={12} display="flex" justifyContent="flex-end"><a className='git-button' href={gitLink}>Git Code<img alt='git-logo' src={github} /></a></Grid>
+    </Grid>
+}
+
+export default Example;
